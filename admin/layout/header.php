cd43dbcfc1c3d4d31071e389932312617a0f5509
thesubod1 <thesubodh1@gmail.com>
2022-04-30 10:43:11 +0545

    initial commit

---
+++ admin/layout/header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Tourism Homepage</title>
+        <link rel = "stylesheet" type = text/css href="../../assets/css/header.css"/>
+    </head>
+    <body>
+
+    <div class="nav-menu">
+        <div id="menu" onclick="togglebar()">
+            <div class="bar" id="bar-1"></div>
+            <div class="bar" id="bar-2"></div>
+        </div>
+    </div>
+
+        
+        <div class="nav-section" >
+            <div class="nav-wrapper"id="nav" >
+                <ul >
+                    <a href="#"><li>Home</li></a>
+                    <a href="#"><li>Places</li></a>
+                    <a href ="#"><li>Bucket-lists</li></a>
+                    <a href="#"><li>About</li></a>
+                    <a href ="#"><li>Login</li></a>
+                </ul>
+            </div><!-- nav-wrapper -->
+        </div> <!-- /nav-section -->
+
+        <script src="../../assets/js/script.js"></script>
+    </body>
+</html>
