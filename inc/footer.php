ad1f6a0d2ff0f65e84d6c232c7733c3f197689f6
kalpanachamling99 <kalpanachamling99@gmail.com>
2022-09-28 17:51:42 +0545

    fix header

--- inc/footer.php
+++ inc/footer.php
@@ -1,2 +1,16 @@
-</body>
-    </html>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js"></script>
+	<script>
+		$(document).ready(function(){
+			$(".adv-slider").slick({
+				slidesToShow: 2,
+				slideToScroll: 1,
+				autoplay: true,
+				dots: false,
+				arrow: false,
+			});
+		});
+	</script>
+    </body>
+</html>
--- inc/header.php
+++ inc/header.php
@@ -4,33 +4,40 @@
 ?>
 <html>
     <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?php echo site_url().'/assets/css/header.css'; ?>" />
-    <link rel="stylesheet" href="<?php echo site_url().'/assets/css/mobilemenu.css'; ?>" />
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="<?php echo site_url().'/assets/css/grid.css'; ?>" />
+        <link rel="stylesheet" href="<?php echo site_url().'/assets/css/header.css'; ?>" />
+        <link rel="stylesheet" href="<?php echo site_url().'/assets/css/mobilemenu.css'; ?>" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css">
+        <link rel="stylesheet" href="<?php echo site_url().'/assets/css/index.css'; ?>" />
     </head>
 <body>
-    <header class="site-url">
-        <div class="nav-container">
-            <nav class="site-navigation desktop">
-                <ul class="primary-menu" id="primary-menu">
-                    <li><a href="<?php echo site_url().'/index.php'; ?>">Home</a></li>
-                    <li><a href="<?php echo site_url().'/places.php'; ?>">Places</a></li>
-                    <li><a href="<?php echo site_url().'/adventures.php';?>">Adventures</a></li>
-                    <li><a href="#">Blog</a></li>
-
-                    <div class="side-nav">
-                        <div class="side-nav-container">
-                            <div class="search-icon"><ion-icon name="search"></ion-icon></div>
-                            <div class="login-icon" id = "deactive"><ion-icon name="person"></ion-icon></div></button>
-                            <div class="login-img" id = "active" style="background-image:url(<?php echo site_url().'../pictures/adventure.jpg'; ?>)"></div>
-                            <div class="map-icon" style="background-image:url(<?php echo site_url().'../pictures/map.jpg'; ?>)"></div>
-                        </div>
-                    </div>
-                </ul>
-            </nav>
+    <header class="site-url-- site-header">
+        <div class="nav-container-- container">
+            <div class="d-flex wrapper">
+                <nav class="site-navigation desktop">
+                    <ul class="primary-menu" id="primary-menu">
+                        <li><a href="<?php echo site_url().'/index.php'; ?>">Home</a></li>
+                        <li><a href="<?php echo site_url().'/places.php'; ?>">Places</a></li>
+                        <li><a href="<?php echo site_url().'/adventures.php';?>">Adventures</a></li>
+                        <li><a href="#">Blog</a></li>
+                    </ul>
+                </nav>
+                <div class="side-nav-- site-feature">
+                    <ul>
+                        <li class="search-icon"><ion-icon name="search"></ion-icon></li>
+                        <li class="login-icon" id = "deactive"><ion-icon name="person"></ion-icon></li>
+                        <li class="login-img" id = "active">
+                            <img src="<?php echo site_url().'/pictures/adventure.jpg'; ?>">
+                        </li>
+                        <li class="map-icon">
+                            <img src="<?php echo site_url().'/pictures/map.jpg'; ?>" alt="">
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </div>
     </header>
-<script src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons.js"></script>
 
